c7985609ecc3f871be9bba63d8e770bde6748831
Md. Abid Ullah Muhib <usshas@Mds-MacBook-Pro.local>
2024-09-26 04:37:58 +0600

    Day 1

---
+++ theory/_01_MyFirstProgram.java
@@ -0,0 +1,13 @@
+package theory;
+public class _01_MyFirstProgram {
+ public static void main(String[] args) {
+    System.out.println("Assalamu Olaykum");
+    return;
+  }
+}
+
+class _01_MyFirstClass {
+  public static void main(String[] args) {
+    System.out.println("How are you?");
+  }
+}
